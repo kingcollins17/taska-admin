7c0e5187efef7b2647832530db9199081e084f77
Collins <kingcollins172@gmail.com>
2026-09-11 09:01:50 +0100

    first commit

---
+++ lib/components/counter.dart
@@ -0,0 +1,42 @@
+import 'package:jaspr/dom.dart';
+import 'package:jaspr/jaspr.dart';
+
+import 'embedded_counter.dart';
+
+class Counter extends StatefulComponent {
+  const Counter({super.key});
+
+  @override
+  State<Counter> createState() => CounterState();
+}
+
+class CounterState extends State<Counter> {
+  int count = 0;
+
+  @override
+  Component build(BuildContext context) {
+    return div([
+      div(classes: 'counter', [
+        button(
+          onClick: () {
+            setState(() => count--);
+          },
+          [.text('-')],
+        ),
+        span([.text('$count')]),
+        button(
+          onClick: () {
+            setState(() => count++);
+          },
+          [.text('+')],
+        ),
+      ]),
+      EmbeddedCounter(
+        count: count,
+        onChange: (value) {
+          setState(() => count = value);
+        },
+      ),
+    ]);
+  }
+}
